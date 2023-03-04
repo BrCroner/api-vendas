8235478148f0981c7bea4ecf4dcd3c31e71926aa
BrCroner <br.croner@gmail.com>
2023-03-03 22:05:19 -0300

    Adicionando bcryptjs

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import User from '../typeorm/entities/Users';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
   name: string;
@@ -16,10 +17,11 @@ class CreateUserService {
     if (emailExists) {
       throw new AppError('Email informado já está em uso!');
     }
+    const hashedPassword = await hash(password, 8);
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     await usersRepository.save(user);
     return user;
