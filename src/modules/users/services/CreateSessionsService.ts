c5d5f3ede2a1cbc4aa52be6a1ca524ba46952b02
BrCroner <br.croner@gmail.com>
2023-03-03 22:59:53 -0300

    Criando CreateSessionsService.ts

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/Users';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Email ou senha inválidas!', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+    if (!passwordConfirmed) {
+      throw new AppError('Email ou senha inválidas!', 401);
+    }
+
+
+    return user;
+}
+
+export default CreateSessionsService;
