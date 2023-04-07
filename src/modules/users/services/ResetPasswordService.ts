b576e828965696a37b29b3c6ced24f6c942f09e5
BrCroner <br.croner@gmail.com>
2023-04-07 07:27:30 -0300

    Corrigindo indentação.

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,38 +1,38 @@
 import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
-import { hash } from 'bcryptjs'
-import { isAfter, addHours } from 'date-fns'
+import { hash } from 'bcryptjs';
+import { isAfter, addHours } from 'date-fns';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import UsersTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 
 interface IRequest {
   token: string;
-  password: string
+  password: string;
 }
 class ResetPasswordService {
   public async execute({ token, password }: IRequest): Promise<void> {
     const usersRepository = getCustomRepository(UsersRepository);
-    const usersTokensRepository = getCustomRepository(UsersTokensRepository)
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
 
-    const userToken = await usersTokensRepository.findByToken(token)
-    if(!userToken) {
-      throw new AppError('O token de usuário é inexistente.')
+    const userToken = await userTokensRepository.findByToken(token);
+    if (!userToken) {
+      throw new AppError('O token de usuário é inexistente.');
     }
 
-    const user = await usersRepository.findById(userToken.user_id)
-    if(!user) {
-      throw new AppError('Usuário inexistente!')
+    const user = await usersRepository.findById(userToken.user_id);
+    if (!user) {
+      throw new AppError('Usuário inexistente!');
     }
 
-    const tokenCreatedAt = userToken.created_at
-    const compareDate = addHours(tokenCreatedAt, 2)
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
 
-    if(isAfter(Date.now(), compareDate)) {
-      throw new AppError('Token expirado!')
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expirado!');
     }
 
-    user.password = await hash(password, 8)
-
+    user.password = await hash(password, 8);
+  }
 }
 
 export default ResetPasswordService;
